0bce3c213b90bdbcfce94bf5e013458643ea9a47
Shawn <shayanalizadeh@outlook.com>
2024-11-19 14:42:32 +0000

    intro page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:mongo_mate/screens/intro.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:mongo_mate/helpers/toast.dart';
 import 'package:mongo_mate/screens/home.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -10,7 +12,6 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   MobileAds.instance.initialize();
-  AdRepository.showConsentUMP();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -28,15 +29,65 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        scaffoldMessengerKey: ToastHelper.scaffoldMessengerKey,
-        home: const HomePage(),
-        theme: ThemeData(
+      debugShowCheckedModeBanner: false,
+      scaffoldMessengerKey: ToastHelper.scaffoldMessengerKey,
+      home: const InitialScreen(),
+      theme: ThemeData(
           brightness: Brightness.light,
-        ),
-        darkTheme: ThemeData(
+          colorScheme: const ColorScheme.light(
+              primary: Colors.deepOrange,
+              secondary: Colors.deepOrangeAccent,
+              onInverseSurface: Color.fromARGB(255, 242, 242, 242))),
+      darkTheme: ThemeData(
           brightness: Brightness.dark,
+          colorScheme: const ColorScheme.dark(
+              primary: Colors.orange,
+              secondary: Colors.orangeAccent,
+              onInverseSurface: Color.fromARGB(255, 27, 27, 27))),
+      themeMode: ThemeMode.system,
+    );
+  }
+}
+
+class InitialScreen extends StatefulWidget {
+  const InitialScreen({Key? key}) : super(key: key);
+
+  @override
+  State<InitialScreen> createState() => _InitialScreenState();
+}
+
+class _InitialScreenState extends State<InitialScreen> {
+  bool _isLoading = true;
+  bool _isIntroSeen = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkIntroStatus();
+  }
+
+  Future<void> _checkIntroStatus() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool seen = prefs.getBool('intro_seen') ?? false;
+
+    setState(() {
+      _isIntroSeen = seen;
+      _isLoading = false; // Loading complete
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Show a loading screen while checking the intro status
+    if (_isLoading) {
+      return const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
         ),
-        themeMode: ThemeMode.system);
+      );
+    }
+
+    // Navigate to the appropriate page based on the intro status
+    return _isIntroSeen ? const HomePage() : const IntroPage();
   }
 }
---
+++ lib/screens/intro.dart
@@ -0,0 +1,233 @@
+import 'package:app_tracking_transparency/app_tracking_transparency.dart';
+import 'package:flutter/material.dart';
+import 'package:mongo_mate/utilities/AdRepository.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:mongo_mate/screens/home.dart';
+
+class IntroPage extends StatefulWidget {
+  const IntroPage({Key? key}) : super(key: key);
+
+  @override
+  State<IntroPage> createState() => _IntroPageState();
+}
+
+class _IntroPageState extends State<IntroPage> {
+  final PageController _pageController = PageController();
+  final TextEditingController _nameController = TextEditingController();
+  int _currentPage = 0;
+  // bool _isNameValid = false;
+  // bool _nameSubmitted = false;
+  bool _continued = false;
+  bool _showPrivacyPage = false;
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    _nameController.dispose();
+    super.dispose();
+  }
+
+  // void _submitName() {
+  //   if (_nameController.text.trim().length >= 2) {
+  //     setState(() {
+  //       _nameSubmitted = true;
+  //     });
+  //     Future.delayed(const Duration(seconds: 2), () {
+  //       setState(() {
+  //         _showPrivacyPage = true;
+  //       });
+  //     });
+  //   } else {
+  //     setState(() {
+  //       _isNameValid = false;
+  //     });
+  //   }
+  // }
+
+  void _continue() {
+    setState(() {
+      _continued = true;
+    });
+    setState(() {
+      _showPrivacyPage = true;
+    });
+  }
+
+  void _completeIntro() async {
+    AdRepository.showConsentUMP();
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('intro_seen', true);
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => const HomePage()),
+    );
+  }
+
+  void _requestATT() async {
+    if (await AppTrackingTransparency.trackingAuthorizationStatus ==
+        TrackingStatus.notDetermined) {
+      await Future.delayed(const Duration(milliseconds: 200));
+      await AppTrackingTransparency.requestTrackingAuthorization();
+    }
+    _completeIntro();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _showPrivacyPage
+          ? SafeArea(child: _buildPrivacyPage())
+          : _continued
+              ? SafeArea(child: _buildWelcomeAnimation())
+              : SafeArea(child: _buildOnboarding()),
+    );
+  }
+
+  Widget _buildOnboarding() {
+    return Column(
+      children: [
+        Expanded(
+          child: PageView(
+            controller: _pageController,
+            onPageChanged: (index) {
+              setState(() {
+                _currentPage = index;
+              });
+            },
+            children: [
+              _buildPage(
+                image: Icons.storage_rounded,
+                title: "Welcome to MonMate",
+                description: "Manage MongoDB with MonMate!",
+              ),
+              _buildPage(
+                image: Icons.cable_rounded,
+                title: "Database Management",
+                description:
+                    "Create, edit, and delete connections, collections, and documents. Enjoy built-in editor, sorting, and filtering.",
+              ),
+              _buildPage(
+                image: Icons.search_rounded,
+                title: "Query Sorting and Filtering",
+                description:
+                    "Run your queries with precision. Sort and filter your data to quickly find exactly what you're looking for. MonMate's powerful querying capabilities make data retrieval efficient and effective.",
+              ),
+            ],
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List.generate(3, (index) {
+            return Container(
+              margin: const EdgeInsets.all(4),
+              width: 8,
+              height: 8,
+              decoration: BoxDecoration(
+                color:
+                    _currentPage == index ? Colors.orangeAccent : Colors.grey,
+                shape: BoxShape.circle,
+              ),
+            );
+          }),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              // TextField(
+              //   controller: _nameController,
+              //   onChanged: (value) {
+              //     setState(() {
+              //       _isNameValid = value.trim().length >= 2;
+              //     });
+              //   },
+              //   decoration: InputDecoration(
+              //     labelText: "Enter your name",
+              //     border: OutlineInputBorder(),
+              //   ),
+              // ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: _continue,
+                child: const Text("Continue"),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildPage({
+    required IconData image,
+    required String title,
+    required String description,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(image, size: 100, color: Colors.orangeAccent),
+          const SizedBox(height: 24),
+          Text(
+            title,
+            style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 16),
+          Text(
+            description,
+            style: const TextStyle(fontSize: 16),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildWelcomeAnimation() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Icon(Icons.check_circle, size: 100, color: Colors.green),
+          const SizedBox(height: 24),
+          Text(
+            "Welcome, ${_nameController.text.trim()}!",
+            style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 16),
+          const Text("Let’s set up your privacy preferences."),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPrivacyPage() {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Icon(Icons.privacy_tip, size: 100, color: Colors.orangeAccent),
+          const SizedBox(height: 24),
+          const Text(
+            "Privacy Settings",
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 16),
+          const Text(
+            "To keep this app free, we'd like your permission to track your activity for personalized ads.",
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 24),
+          ElevatedButton(
+            onPressed: _requestATT,
+            child: const Text("Continue"),
+          ),
+        ],
+      ),
+    );
+  }
+}
